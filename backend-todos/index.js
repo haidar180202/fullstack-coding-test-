c278dbfb2053bb4e7bb0757ecb70830bf2444502
haidar180202 <gshaidar6@gmail.com>
2025-11-08 17:59:31 +0700

    feat(backend-todos): add configuration for authentication and validation jwt

--- backend-todos/index.js
+++ backend-todos/index.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const todoRoutes = require('./routes/todoRoutes');
+const authRoutes = require('./routes/authRoutes');
+const authMiddleware = require('./middleware/auth');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swagger');
 
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use('/todos', todoRoutes);
+app.use('/auth', authRoutes);
+app.use('/todos', authMiddleware, todoRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
